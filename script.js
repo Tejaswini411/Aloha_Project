acc6079d335cc03de09b7700d4996e7d85381d8e
Tejaswini411 <kumartejaswini4@gmail.com>
2020-02-06 20:19:39 -0800

    email verification using javascript

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-function Subscribe()
-{ 
-   alert ("Thanks for subscribing!"); 
+document.addEventListener("DOMContentLoaded", function () {
 
-} 
+  let inputEmail = document.getElementById('signup-form');
+  
+  inputEmail.addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    const text = document.getElementById("input");
+    const trimVal = text.value.trim();
+    if (trimVal === "") {
+      alert("Please submit a valid email address.");
+    }
+    
+    else if (trimVal.length >= 0) {
+      alert("Thanks for subscribing!");
+    }
+    
+    text.value = "";
+    
+  });
 
+  $(document).ready(function(){
+    $('.productwrapper').slick({
+      dots: true, autoplay: true, infinite: false, slidesToShow: 4,
+      slidesToScroll: 1, 
+      responsive : [{ breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      }]
+      
+    });
+  });
 
-
-
-
-
-
-
-
-            $(document).ready(function(){
-                $('.productwrapper').slick({
-                    dots: true, autoplay: true, infinite: false, slidesToShow: 4,
-                    slidesToScroll: 1, 
-                    responsive : [{ breakpoint: 800,
-                        settings: {
-                            slidesToShow: 2,
-                        }
-                        },
-                        {
-                            breakpoint: 600,
-                            settings: {
-                            slidesToShow: 1,
-                            }
-
-                    }]
-
-                });
-            });
-
-
-
+});
